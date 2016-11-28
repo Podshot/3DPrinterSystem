683c8953fc3ecb1563db3da4dded5e400679f2b4
Podshot <ben.gothard3@gmail.com>
2016-11-28 07:01:06 -0500

    Improved API wrapper and UI templates

--- Client/api_wrapper.py
+++ Client/api_wrapper.py
@@ -2,19 +2,32 @@ import requests
 
 connection_pointer = "printer-system-test.herokuapp.com"
 
-login_payload = {
-                 }
-
-auth_id = login_response = requests.post("http://{}/api/authenticate".format(connection_pointer), data=login_payload)
-if login_response.status_code == 405:
-    auth_id = -1
-else:
-    auth_id = login_response.json()['auth_id']
-
-def get_all_submissions():
-    all_response = requests.get("http://{}/api/submission/get_all".format(connection_pointer), data={'auth_id': auth_id, 'with_data': True})
-    if all_response.status_code == 405:
-        return []
-    else:
-        return all_response.json()['submissions']
+class APIWrapper(object):
     
+    def __init__(self, username, password):
+        login_payload = {
+                 'username': username,
+                 'password': password
+                 }
+        self._username = username
+        response = requests.post("http://{}/api/authenticate".format(connection_pointer), data=login_payload).json()
+        if response['auth_id'] == -1:
+            print "Login failed..."
+            self._failed_login = True
+        else:
+            print "Login succeeded..."
+            self._failed_login = False
+        self._auth_id = response['auth_id']
+        
+    def get_username(self):
+        return self._username
+        
+    def login_failed(self):
+        return self._failed_login
+    
+    def get_all_submissions(self):
+        all_response = requests.get("http://{}/api/submission/get_all".format(connection_pointer), data={'auth_id': self._auth_id, 'with_data': True})
+        if all_response.status_code == 405:
+            return []
+        else:
+            return all_response.json()['submissions']
--- Client/client.py
+++ Client/client.py
@@ -4,18 +4,48 @@ import pyside_dynamic
 import api_wrapper
 import custom_widgets
 
+class LoginDialog(QtGui.QDialog):
+    
+
+    def login(self):
+        username = self.username_field.text()
+        password = self.password_field.text()
+        wrapper = api_wrapper.APIWrapper(username, password)
+        if wrapper.login_failed():
+            self.failed_label.setVisible(True)
+        else:
+            self.parent()._wrapper = wrapper
+            self.close()
+    
+    
+    def init_ui(self):
+        self.failed_label.setVisible(False)
+        
+        self.setGeometry(self.parent().x(), self.parent().y(), 400, 200)
+        self.login_button.clicked.connect(self.login)
+        self.cancel_button.clicked.connect(self.close)
+    
+    
+    def __init__(self, parent=None):
+        super(LoginDialog, self).__init__(parent)
+        
+        pyside_dynamic.loadUi('Login_Dialog.ui', self)
+        
+        self.init_ui()
+
 class MainWindow(QtGui.QMainWindow):
-    default_title_text = '<b>Submission Title:</b>'
+    default_title_text = '<b>Submission Title:</b> '
+    default_status_text = '<b>Status:</b> '
 
     def cell_selected(self, row, column):
         data = self.submissions[row]
         
-        self.title_label.setText(self.default_title_text + " " + data["title"])
+        self.title_label.setText(self.default_title_text + data["title"])
         self.rafts_option.setChecked(data['options']['rafts'])
         self.supports_option.setChecked(data['options']["supports"])
-    
-    def init_ui(self):
-        self.submissions = api_wrapper.get_all_submissions()
+        
+    def update_table(self):
+        self.submissions = self._wrapper.get_all_submissions()
         
         self.submissions_table.setRowCount(len(self.submissions))
         for row in xrange(self.submissions_table.rowCount()):
@@ -26,8 +56,27 @@ class MainWindow(QtGui.QMainWindow):
             self.submissions_table.setItem(row, 0, item_title)
             self.submissions_table.setItem(row, 1, item_priority)
             
-        self.submissions_table.cellClicked.connect(self.cell_selected)
+    
+    def login(self):
+        dialog = LoginDialog(self)
         
+        dialog.exec_()
+        
+        if self._wrapper:
+            self.actionSignIn.setVisible(False)
+            self.actionSignOut.setVisible(True)
+            self.statusBar.showMessage('Logged in as: {}'.format(self._wrapper.get_username()))
+            self.parent().update_table()
+    
+    
+    def init_ui(self):
+        self.statusBar.showMessage('Not logged in')
+        
+        self.actionSignIn.triggered.connect(self.login)
+        
+        self.actionExit.triggered.connect(self.close)
+        
+        self.submissions_table.cellClicked.connect(self.cell_selected)
         self.test_button.clicked.connect(self.on_test_button_clicked)
         
         self.show()
@@ -38,7 +87,9 @@ class MainWindow(QtGui.QMainWindow):
         custom_widgets_map = {
                           'QReadOnlyCheckBox': custom_widgets.QReadOnlyCheckBox
                           }
-        pyside_dynamic.loadUi("untitled.ui", self, custom_widgets_map)
+        pyside_dynamic.loadUi("Main_Window.ui", self, custom_widgets_map)
+        
+        self._wrapper = None
         
         self.submissions = []
         
